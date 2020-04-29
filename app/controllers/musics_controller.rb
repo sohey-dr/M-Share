081c29a2c8ccb22c6ae5d1545de24520c4ec1486
sohei.hashimoto <heyso1209@gmail.com>
2020-04-29 19:19:52 +0000

    musics index

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -2,6 +2,12 @@ class MusicsController < ApplicationController
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
   
+  def index
+    if params[:search].present?
+    @searchartists = RSpotify::Track.search(params[:search])
+    end
+  end
+  
   def search
     if params[:search].present?
     @searchartists = RSpotify::Artist.search(params[:search])
