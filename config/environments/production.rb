78871320ff40c4af862a05e65cffe079df604a5e
sohei.hashimoto <heyso1209@gmail.com>
2020-05-07 12:13:03 +0000

    fix error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-config.assets.initialize_on_precompile = false
