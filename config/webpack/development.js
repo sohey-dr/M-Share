7590ca90df3b9b8176da51c621e5a8c91a2c2071
sohei.hashimoto <heyso1209@gmail.com>
2020-05-07 12:10:42 +0000

    fix error

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,10 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports = Object.assign({}, environment.toWebpackConfig(), {
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  }
-})
+module.exports = environment.toWebpackConfig()
